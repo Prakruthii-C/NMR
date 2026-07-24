b0d556e838d657a3fe678ae95ee292b84bdff278
Prakruthii-C <prakruthi.c2913@gmail.com>
2026-07-24 16:51:57 +0530

    fixed generate_spectra.py

--- nmr-backend/data/generate_spectra.py
+++ nmr-backend/data/generate_spectra.py
@@ -19,32 +19,46 @@ def generate_mock_spectrum(mock_peaks,num_points=1000):
 if __name__=="__main__":
     num_samples=500
     all_spectra=[]
+    all_labels=[] # to track target variables
 
     for i in range(num_samples):
-        # slightly randomise peak configurations
-        peak1_pos=np.random.uniform(1.1,1.3)
-        peak2_pos=np.random.uniform(3.4,3.6)
-        peak3_pos=np.random.uniform(7.0,7.4)
+        current_peaks=[]
+        # 50% chance if peak is present or not
+        has_peak1 = np.random.choice([0, 1])
+        has_peak2 = np.random.choice([0, 1])
+        has_peak3 = np.random.choice([0, 1])
+        
+        if has_peak1:
+            peak1_pos = np.random.uniform(1.1, 1.3)
+            amp1 = np.random.uniform(0.8, 1.2)
+            current_peaks.append((peak1_pos, 0.05, amp1))
+            
+        if has_peak2:
+            peak2_pos = np.random.uniform(3.4, 3.6)
+            amp2 = np.random.uniform(0.5, 0.8)
+            current_peaks.append((peak2_pos, 0.08, amp2))
+            
+        if has_peak3:
+            peak3_pos = np.random.uniform(7.0, 7.4)
+            amp3 = np.random.uniform(0.3, 0.6)
+            current_peaks.append((peak3_pos, 0.12, amp3))
 
-        amp1=np.random.uniform(0.8,1.2)
-        amp2=np.random.uniform(0.5,0.8)
-        amp3=np.random.uniform(0.3,0.6)
-
-        current_peaks=[
-            (peak1_pos,0.05,amp1),
-            (peak2_pos,0.08,amp2),
-            (peak3_pos,0.12,amp3)
-            ]
         intensity=generate_mock_spectrum(current_peaks)
         all_spectra.append(intensity)
+        all_labels.append([has_peak1, has_peak2, has_peak3])
     
-    columns=[f'point_{j}' for j in range(1000)]
-    df=pd.DataFrame(all_spectra,columns=columns)
+    feature_columns=[f'point_{j}' for j in range(1000)]
+    df_features=pd.DataFrame(all_spectra,columns=feature_columns)
+
+    label_columns=['group_1','group_2','group_3']
+    df_labels=pd.DataFrame(all_labels,columns=label_columns)
+
+    df_final=pd.concat([df_features,df_labels],axis=1)
 
     output_dir="nmr-backend/data"
     os.makedirs(output_dir,exist_ok=True)
     output_path = os.path.join(output_dir, "simulated_spectra.csv")
-    df.to_csv(output_path, index=False)
+    df_final.to_csv(output_path, index=False)
 
     
 
